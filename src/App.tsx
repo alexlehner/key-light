16f4e99ff9e5530f4511f2c8765135920d09da8b
Alex Lehner <alex@alexlehner.com>
2022-02-13 08:53:46 -0500

    feat: Create buttons for key temps

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,64 @@
-import "./styles.css";
+import {useState, useEffect} from 'react';
 import { colorTemperature2rgb } from "color-temperature";
 
-const temps = [5000, 4000, 3500, 3000, 2700];
+import "./styles.css";
+
+/**
+ * Update the background color of the appropriate dom node
+ *
+ * @param color string
+ */
+const setBackgroundColor = (color: string) => {
+  const rootElement = document.getElementById("root");
+  if (rootElement) {
+    rootElement.style.setProperty(
+      "--color-selected-temperature",
+      color
+    );
+  }
+}
+
+/**
+ * Generate a CSS-friendly string representing the color we want to set the background
+ *
+ * @param value number
+ * @returns string
+ */
+const getRgbFromKelvin = (value: number) => {
+  if (value == 0) {
+    return 'transparent';
+  }
+
+  const temperature = colorTemperature2rgb(value);
+  return `rgb(${temperature.red}, ${temperature.green}, ${temperature.blue})`;
+}
 
 export default function App() {
-  const handleClick = () => {
-    let temperature = colorTemperature2rgb(
-      temps[Math.floor(Math.random() * temps.length)]
-    );
-    console.log(temperature);
+  const [temp, setTemp] = useState(0);
 
-    const rootElement = document.getElementById("root");
-    if (rootElement) {
-      rootElement.style.setProperty(
-        "--color-selected-temperature",
-        `rgb(${temperature.red}, ${temperature.green}, ${temperature.blue})`
-      );
-    }
-  };
+  useEffect(() => {
+    setBackgroundColor(getRgbFromKelvin(temp));
+  }, [temp]);
 
   return (
     <div className="App">
-      <button className="cta" onClick={handleClick}>
-        Shuffle temperature
+      <button className="cta" onClick={() => setTemp(2700)}>
+        Set Temp 2700
+      </button>
+      <button className="cta" onClick={() => setTemp(3000)}>
+        Set Temp 3000
+      </button>
+      <button className="cta" onClick={() => setTemp(3500)}>
+        Set Temp 3500
+      </button>
+      <button className="cta" onClick={() => setTemp(4000)}>
+        Set Temp 4000
+      </button>
+      <button className="cta" onClick={() => setTemp(5000)}>
+        Set Temp 5000
+      </button>
+      <button className="cta" onClick={() => setTemp(0)}>
+        Off
       </button>
     </div>
   );
